61b8345a1b78d54945dab87d872fad7cc4968b23
carlosantiquata <a.terrao-beca@cfacampusmontsouris.fr>
2026-04-01 17:27:11 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,31 @@
 variable "render_api_key" {
-  type      = string
-  sensitive = true
+  description = "Clé API Render"
+  type        = string
+  sensitive   = true
 }
 
 variable "render_owner_id" {
-  type = string
+  description = "Owner ID Render"
+  type        = string
+}
+
+variable "database_url" {
+  description = "URL de connexion PostgreSQL"
+  type        = string
+  sensitive   = true
 }
 
 variable "image_url" {
-  type = string
+  description = "URL de l'image Docker (injectée par la CI)"
+  type        = string
 }
 
 variable "image_tag" {
-  type = string
+  description = "Tag de l'image Docker (injectée par la CI)"
+  type        = string
+}
+
+variable "github_actor" {
+  description = "GitHub username"
+  type        = string
 }
